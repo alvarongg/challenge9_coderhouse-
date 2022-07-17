faf4e244bdf3bd14fdb86defc540ecbcbec3e08e
Alvaro Garcia <alvarogarciainfo@gmail.com>
2022-07-17 19:01:39 -0300

    todo: add passport and mongo-connect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require('path');
-const moment = require('moment'); 
-const {productRouter,getAllProd,saveProd }= require("./productRouter.js");
+const path = require("path");
+const moment = require("moment");
+const { productRouter, getAllProd, saveProd } = require("./productRouter.js");
 const productRouterTest = require("./productRouter-test.js");
 const { engine } = require("express-handlebars");
-const { Server: HttpServer } = require('http');
-const { Server: SocketServer } = require('socket.io');
+const { Server: HttpServer } = require("http");
+const { Server: SocketServer } = require("socket.io");
 //const MensajesSqlite = require("./mensajes.js");
 const MensajesFs = require("./mensajes.js");
 
@@ -13,8 +13,8 @@ const MensajesFs = require("./mensajes.js");
 // const { optionsSqlite } = require(options_path);
 const app = express();
 
-const tabla_chat = 'mensajes';
-let options_path = path.join(__dirname,'..', 'DB','mensajes.json');
+const tabla_chat = "mensajes";
+let options_path = path.join(__dirname, "..", "DB", "mensajes.json");
 //let chat = new MensajesSqlite(optionsSqlite,tabla_chat);
 let chat = new MensajesFs(options_path);
 
@@ -31,20 +31,19 @@ app.use(
   })
 );
 
-
-let views_path = path.join(__dirname, '..', 'views');
-app.use(express.static('public'));
+let views_path = path.join(__dirname, "..", "views");
+app.use(express.static("public"));
 
 app.engine(
-  'hbs',
+  "hbs",
   engine({
-    extname: '.hbs',
-    defaultLayout: 'index.hbs',
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
   })
 );
 
-app.set('views', views_path);
-app.set('view engine', 'hbs');
+app.set("views", views_path);
+app.set("view engine", "hbs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -52,14 +51,13 @@ app.use(express.json());
 //app.use("/api/productos", productRouter);
 app.use("/api/productos-test", productRouterTest);
 
+app.get("/", (req, res) => {
+  res.render("main");
+});
 
-app.get('/', (req, res) => {
-    res.render('main');
-  });
-
-  app.get('/chat', (req, res) => {
-    res.render('new_chat');
-  });
+app.get("/chat", (req, res) => {
+  res.render("new_chat");
+});
 
 // app.listen(8080, () => {
 //   console.log("Estoy escuchando 8080 --engine handlebars");
@@ -68,31 +66,33 @@ app.get('/', (req, res) => {
 const httpServer = new HttpServer(app);
 const socketServer = new SocketServer(httpServer);
 
-socketServer.on('connection', async (socket) => {
-
+socketServer.on("connection", async (socket) => {
   //emite los mensajes y productos actuales
-  socket.emit('messages', await chat.normalize());
-  socket.emit('products', await getAllProd());
+  socket.emit("messages", await chat.normalize());
+  socket.emit("products", await getAllProd());
 
   // socket.on('new_product', async  (producto) => {
-  //   //inserta el producto que le llego 
+  //   //inserta el producto que le llego
   //   console.log(`saved prod: ${producto}`);
   //    saveProd(producto);
   //   socketServer.sockets.emit('products', await getAllProd());
-  
+
   // });
 
-  
-  socket.on('new_message',async (mensaje) => {
-    console.log('ejecutando  guardado de mensaje');
-    await  chat.save(mensaje);
-    console.log('nuevo mensaje guardado');
-    socketServer.sockets.emit('messages', await chat.normalize());
- 
+  socket.on("new_message", async (mensaje) => {
+    console.log("ejecutando  guardado de mensaje");
+    await chat.save(mensaje);
+    console.log("nuevo mensaje guardado");
+    socketServer.sockets.emit("messages", await chat.normalize());
   });
-  
 });
 
-httpServer.listen(8080, () => {
-  console.log('Estoy escuchando en el puerto 8080');
+//Manejador de errores
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send("Ocurrio un error: " + err);
+});
+
+httpServer.listen(process.env.PORT || 8080, () => {
+  console.log("Estoy escuchando en el puerto 8080");
 });
